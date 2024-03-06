603f332b7ee522e19e89aca27eadfe2439560610
Khalid Oqaidi <70282956+oqaidik@users.noreply.github.com>
2024-03-06 11:47:18 +0100

    Update page1.py

--- page1.py
+++ page1.py
@@ -1,31 +1,5 @@
 # Import Streamlit
 import streamlit as st
 
-# Set page configuration
-st.set_page_config(
-    page_title="Multipage App",
-    page_icon="📑",
-)
-st.image("patcha.jpg")
-
-
-# Main Page
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
-
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
-
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered:", my_input)
-
-# Projects Page
-st.title("Projects")
-st.write("You have entered:", st.session_state["my_input"])
-
-# Contact Page
-st.title("Contact")
+st.write("Test")
+st.radio("MENU",["Home","Contact","Projects"])
